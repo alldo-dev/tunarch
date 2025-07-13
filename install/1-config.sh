7213ece98aa609f78a8e261c4cb731f44241e142
alldo-dev <alldo.world@gmail.com>
2025-07-13 22:27:28 +0200

    modded waybar styles and conf

--- install/1-config.sh
+++ install/1-config.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 source ~/.acr/utils.sh
+
+_logColor "$cyan" "$log_header" "we need to add your user to the input group"
+sudo usermod -a -G input $USER
+
 _logColor "$cyan" "$log_header" "copying dotfiles to $HOME/.config/"
 
 # Copy the config files
