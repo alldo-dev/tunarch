07820a5a532ec1812f6978c6ff5d43ac889217c3
alldo-dev <alldo.world@gmail.com>
2025-07-15 20:13:10 +0200

    fixed typo in logheader on install scripts

--- install/4-utilities.sh
+++ install/4-utilities.sh
@@ -2,9 +2,9 @@
 
 source ~/.acr/utils.sh
 _logColor "$cyan" "$log_header" "Installing utility software:"
-_logColor "$magenta" "\[utilities\]" "Installing utility software:"
-_logColor "$magenta" "\[utilities\]" "rofi | application laucher"
-_logColor "$magenta" "\[utilities\]" "waybar | wayland status bar"
+_logColor "$magenta" "utilities" "Installing utility software:"
+_logColor "$magenta" "utilities" "rofi | application laucher"
+_logColor "$magenta" "utilities" "waybar | wayland status bar"
 
 
 yay -S --noconfirm --needed \
