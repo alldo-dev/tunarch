145069785def45cf362af107cbfc7ec57def1284
alldo-dev <alldo.world@gmail.com>
2025-07-15 20:58:20 +0200

    added wl-clipboard and cliphist to utilities

--- install/4-utilities.sh
+++ install/4-utilities.sh
@@ -5,7 +5,13 @@ _logColor "$cyan" "$log_header" "Installing utility software:"
 _logColor "$magenta" "utilities" "Installing utility software:"
 _logColor "$magenta" "utilities" "rofi | application laucher"
 _logColor "$magenta" "utilities" "waybar | wayland status bar"
+_logColor "$magenta" "utilities" "wl-clipboard | cli copy/paste utils for wayland"
+_logColor "$magenta" "utilities" "cliphist | wayland clipboard manager with multimedia support"
 
 
 yay -S --noconfirm --needed \
-  rofi waybar
+  rofi \
+  waybar \
+  wl-clipboard \
+  cliphist
+
