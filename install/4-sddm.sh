0039c3a24c1b3dddfa32633367eef88ec6eea715
alldo-dev <alldo.world@gmail.com>
2025-07-21 15:37:59 +0200

    fixed logging mistake

--- install/4-sddm.sh
+++ install/4-sddm.sh
@@ -3,10 +3,10 @@
 source ~/.acr/utils.sh
 _logColor "$magenta" "sddm - must have" "Installing sddm software:"
 _logColor "$magenta" "sddm - must have" "sddm | simple desktop display manager and login screen"
-_logColor "$magenta" "qt5-graphical effects | provides visual effects for qt user interfaces"
-_logColor "$magenta" "qt5-quickcontrols2 | adds control functionality for qt user interfaces"
-_logColor "$magenta" "qt5-svg | svg renderer for qt user interfaces"
-_logColor "$magenta" "sddm-theme-sugar-candy | sugar candy theme for sddm"
+_logColor "$magenta" "sddm - must have" "qt5-graphical effects | provides visual effects for qt user interfaces"
+_logColor "$magenta" "sddm - must have" "qt5-quickcontrols2 | adds control functionality for qt user interfaces"
+_logColor "$magenta" "sddm - must have" "qt5-svg | svg renderer for qt user interfaces"
+_logColor "$magenta" "sddm - must have" "sddm-theme-sugar-candy | sugar candy theme for sddm"
 
 
 yay -S --noconfirm --needed \
