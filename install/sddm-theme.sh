e6cb00e407ddaeb57905010e4cc47e0ec6d241ae
alldo-dev <alldo.world@gmail.com>
2025-07-21 15:33:53 +0200

    routed the sddm conf to the right place

--- install/sddm-theme.sh
+++ install/sddm-theme.sh
@@ -6,5 +6,5 @@ source ~/.acr/utils.sh
 
 if [ -d /usr/share/sddm/themes/$sddm_theme_name ]; then
     _logColor "$yellow" "sddm-theme" "configuring sddm theme to $sddm_theme_name"
-    sudo cp ~/.acr/sddm/sddm.conf /etc/sddm.conf.d/
+    sudo cp ~/.acr/sddm/sddm.conf /usr/lib/sddm/sddm.conf.d/default.conf
 fi
